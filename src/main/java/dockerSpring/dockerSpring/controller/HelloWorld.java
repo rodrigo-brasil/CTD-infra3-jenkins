5d5779f4a4bba818c839eeaea4e441d5538b982a
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2021-11-17 17:46:01 -0300

    first commit

---
+++ src/main/java/dockerSpring/dockerSpring/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package dockerSpring.dockerSpring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+
+    @GetMapping("/")
+    public String hello(){
+        return  "Ola mundo";
+    }
+}
